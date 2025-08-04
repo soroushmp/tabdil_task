d587ad131231d78e4a0abc86cb05a9d3b6b9807f
soroush moradpour <46293975+soroushmp@users.noreply.github.com>
2025-08-04 03:36:11 +0330

    Update test DB host to 'db' in settings
    Changed the database host from 'postgres' to 'db' in settings_test.py to reflect the correct test environment configuration.

--- Tabdil/settings_test.py
+++ Tabdil/settings_test.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        "HOST": "postgres",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
