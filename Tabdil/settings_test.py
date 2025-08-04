06b1cd802a3e2709cb910c9bee1c9ede6fc8b2b6
soroush moradpour <46293975+soroushmp@users.noreply.github.com>
2025-08-04 03:34:02 +0330

    Add test settings and update stress test instructions
    Introduced Tabdil/settings_test.py for test-specific database configuration using environment variables. Updated stress_test_help.txt to use the new test settings when running Django tests in Docker.

---
+++ Tabdil/settings_test.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        "HOST": "postgres",
+        "PORT": "5432",
+    }
+}
